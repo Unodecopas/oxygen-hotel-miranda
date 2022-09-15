f0841ba647415445f1b6bc8d862f058b392285f2
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-09-15 17:15:37 +0200

    feat: add some images at indexhtml and swiper

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
 const menu = document.querySelector('#menu')
 const nav = document.querySelector('#nav')
 
+
 menu.addEventListener('click', () =>{
   nav.classList.toggle("hidden")
   menu.classList.toggle("menu__active")
+})
+
+const swiper = new Swiper ('.swiper',{
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 })
